9244ad5bd5272c03e3b0012c3c11332d8063aad2
hanheihei <61617063+pencilheart@users.noreply.github.com>
2025-03-04 21:06:22 +0800

    Update Remove Enter in Translate.js

--- Remove Enter in Translate.js
+++ Remove Enter in Translate.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         回车去除
 // @description  自动移除从PDF等复制的文本中包含的换行符，支持谷歌翻译，百度翻译，有道翻译，DeepL翻译，必应翻译，腾讯翻译君
-// @version      3.0
+// @version      3.1
 // @author       Pencilheart
 // @match        https://translate.google.com/*
 // @match        https://translate.google.cn/*
@@ -10,7 +10,7 @@
 // @match        https://www.deepl.com/translator
 // @match        https://cn.bing.com/translator/*
 // @match        https://fanyi.qq.com/*
-// @icon         https://translate.google.cn/favicon.ico
+// @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0Ij48cGF0aCBmaWxsPSJyZWQiIGQ9Ik0xMiAyMS4zNWwtMS40NS0xLjMyQzUuNCAxNS4zNiAyIDEyLjI4IDIgOC41IDIgNS40MiA0LjQyIDMgNy41IDNjMS43NCAwIDMuNDEuODEgNC41IDIuMDlDMTMuMDkgMy44MSAxNC43NiAzIDE2LjUgMyAxOS41OCAzIDIyIDUuNDIgMjIgOC41YzAgMy43OC0zLjQgNi44Ni04LjU1IDExLjU0TDEyIDIxLjM1eiIvPjwvc3ZnPg==
 // @namespace https://greasyfork.org/users/703434
 // ==/UserScript==
 
